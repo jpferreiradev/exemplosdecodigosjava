fa6de4229ef919d6e7bd8dca01a592bc829f30e0
JP Ferreira <jotepauder01@gmail.com>
2022-11-01 14:20:13 -0300

    Update códigos de exemplo Java

--- exemplosDeCodigoJava/src/ExceptionGenerica.java
+++ exemplosDeCodigoJava/src/ExceptionGenerica.java
@@ -1,4 +1,4 @@
-public class MultiplosCatchGenerico {
+public class ExceptionGenerica {
     public static void main(String[] args) {
 
         int [] numeros = {4,8,16,32,64,128};
@@ -8,13 +8,9 @@ public class MultiplosCatchGenerico {
             try {
                 System.out.println(numeros[i]+ "/" + denominadores[i] + " = " + (numeros[i]/denominadores[i]));
             }
-            catch (ArithmeticException e){
-                System.out.println("Erro ao dividir por zero");
-
-            }
-            catch (Throwable e){ // Não usar essa classe, e sim usar a classe Exception quando for uma uma exceção generica
-                System.out.println("Ocorreu um erro");
-
+            catch (Exception e){
+                System.out.println(e.getMessage());
+                e.printStackTrace();
             }
 
         }
