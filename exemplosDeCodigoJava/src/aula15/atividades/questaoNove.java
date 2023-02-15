73a11f97758e9775339501d2390c9a80db91d941
JP Ferreira <jotepauder01@gmail.com>
2023-02-15 11:31:46 -0300

    Update questao da lista

--- exemplosDeCodigoJava/src/aula15/atividades/questaoNove.java
+++ exemplosDeCodigoJava/src/aula15/atividades/questaoNove.java
@@ -1,30 +1,42 @@
 package aula15.atividades;
 
 import java.util.Scanner;
+import java.util.Vector;
 
 public class questaoNove {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        //int vetor[] = new int[3];
+        //int[] numeros = new int[3];
 
         System.out.println("Digite três números:");
 
         System.out.println("Primeiro número:");
         int numeroUm = scan.nextInt();
-        //int numeroUm[0] = scan.nextInt();
         System.out.println("Segundo número: ");
         int numeroDois = scan.nextInt();
         System.out.println("Terceiro número: ");
         int numeroTres = scan.nextInt();
 
-        for(int i = 4;i > 0;i--){
-            //vetor[i] = i;
-            System.out.println(i);
+        if (numeroUm < numeroDois && numeroUm < numeroTres) {
+            System.out.println("Na ordem crescente é essa:" + numeroUm + " < " + numeroDois + " < " + numeroTres);
+        } else if (numeroDois < numeroUm && numeroDois < numeroTres) {
+            System.out.println("Na ordem crescente é essa:" + numeroDois + " < " + numeroUm + " < " + numeroTres);
+        } else if(numeroTres < numeroUm && numeroTres < numeroDois){
+            System.out.println("Na ordem crescente é essa:" + numeroTres + " < " + numeroUm + " < " + numeroDois);
+        }
+
+        if (numeroUm > numeroDois && numeroUm > numeroTres) {
+            System.out.println("Na ordem decrescente é essa:" + numeroUm + " > " + numeroDois + " > " + numeroTres);
+        } else if (numeroDois > numeroUm && numeroDois > numeroTres) {
+            System.out.println("Na ordem decrescente é essa:" + numeroDois + " > " + numeroTres + " > " + numeroUm);
+        } else if(numeroTres > numeroUm && numeroTres > numeroDois){
+            System.out.println("Na ordem decrescente é essa:" + numeroTres + " > " + numeroDois + " > " + numeroUm);
         }
 
     }
-}
+    }
+
 
         /*
         for(int i = 50;i < 100;i++){
@@ -33,3 +45,11 @@ public class questaoNove {
 
         */
 
+        // Forma certa para a questão pedida
+  /*
+        for(int i = 4;i > 0;i--){
+            System.out.println(i);
+        }
+        */
+
+ // - Ordem decresente - Maior para o menor
