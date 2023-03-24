172e5a7b521a3f10eeeeeff6f68f16831cfd65cd
JP Ferreira <jotepauder01@gmail.com>
2023-03-24 10:12:25 -0300

    Update atividades Java

--- exemplosDeCodigoJava/src/aula17/atividades/atividade03.java
+++ exemplosDeCodigoJava/src/aula17/atividades/atividade03.java
@@ -12,86 +12,120 @@ public class atividade03 {
         double salario;
         String sexo;
         String estadoCivil;
+        boolean infoValida = true; // Exemplo de flag
 
 
-        System.out.println("Digite o seu nome: ");
-        nome = scan.nextLine();
-
-        System.out.println("Digite a sua idade: ");
-        idade = scan.nextInt();
-
-        System.out.println("Digte o seu salário: ");
-        salario = scan.nextDouble();
 
 
-        System.out.println("Digite qual é o seu sexo: F ou M: ");
-        sexo = scan.next();
+
+
+
+
 
         //sexo = scan.next().charAt(0);
 
-        System.out.println("Digite o seu estado civil entre:s(solteiro) c( casado)  v( viúdo)   d(divorciado)  ");
-        estadoCivil = scan.next();
-        //estadoCivil = scan.next().charAt(0); // Você tinha utilizado a variavel char
+
+        //estadoCivil = scan.next().charAt(0); // Você queria ter utilizado a variavel char
 
 
-        //System.out.println("--------------");
-        //System.out.println("Seu nome é:" + nome);
-        //System.out.println("Sua idade é:" + idade + " anos");
-        //System.out.println("Seu salário é: R$" + salario + " reais");
-        //System.out.println("Seu sexo é:" + sexo );
-        //System.out.println("Seu estado civil é :" + estadoCivil);
+        infoValida = false;
+        do {
+            System.out.println("Digite o seu nome: ");
+            nome = scan.nextLine();
 
+            if (nome.length() >= 3) {
+                System.out.println("Seu nome é:" + nome);
+                infoValida = true;
+            } else {
+                System.out.println("Digite novamente, é preciso ser maior que 3 caracteres");
+            }
+        } while (!infoValida);
 
-        if (nome.length() >= 3) {
-            System.out.println("Seu nome é:" + nome);
-        } else {
-            System.out.println("Digite novamente, é preciso ser maior que 3 caracteres");
-        }
-        if (idade > 0 && idade < 150) {
-            System.out.println("Sua idade é:" + idade + " anos");
-        } else {
-            System.out.println("Idade não permitida, digite novamente...");
-        }
-        if (salario > 0) {
-            System.out.println("Seu salário é: R$" + salario + " reais");
-        } else {
-            System.out.println("Salário incompátivel,digite novamente... ");
-        }
+        infoValida = false;
+        do {
 
-        if (sexo.equalsIgnoreCase("f") || sexo.equalsIgnoreCase("F")) {
-            System.out.println("Seu sexo é feminino");
-        } else if (sexo.equalsIgnoreCase("m") || sexo.equalsIgnoreCase("M")) {
-            System.out.println("Seu sexo é masculino");
-        } else {
-            System.out.println("Opção errada, digite novamente...");
-        }
+            System.out.println("Digite a sua idade: ");
+            idade = scan.nextInt();
+            if (idade > 0 && idade < 150) {
+                System.out.println("Sua idade é:" + idade + " anos");
+                infoValida = true;
+            } else {
+                System.out.println("Idade não permitida, digite novamente...");
+            }
+        } while (!infoValida);
 
-        switch (estadoCivil) {
-            case "s":
-            case "S":
-                System.out.println("Estado civil: Solteiro");
-                break;
-            case "c":
-            case "C":
-                System.out.println("Estado civil: Casado");
-                break;
-            case "v":
-            case "V":
-                System.out.println("Estado civil: Viúvo");
-                break;
-            case "d":
-            case "D":
-                System.out.println("Estado civil: Divorviado");
-                break;
+        infoValida = false;
+        do {
+            System.out.println("Digte o seu salário: ");
+            salario = scan.nextDouble();
 
-            default:
-                System.out.println("Estado civil inválido");
-        }
+            if (salario > 0) {
+                System.out.println("Seu salário é: R$" + salario + " reais");
+                infoValida = true;
+            } else {
+                System.out.println("Salário incompátivel,digite novamente... ");
+            }
+        } while (!infoValida);
 
+        infoValida = false;
+        do {
+            System.out.println("Digite qual é o seu sexo: F ou M: ");
+            sexo = scan.next();
 
+            if (sexo.equalsIgnoreCase("f") || sexo.equalsIgnoreCase("F")) {
+                System.out.println("Seu sexo é feminino");
+                infoValida = true;
+            } else if (sexo.equalsIgnoreCase("m") || sexo.equalsIgnoreCase("M")) {
+                System.out.println("Seu sexo é masculino");
+                infoValida = true;
+            } else {
+                System.out.println("Opção errada, digite novamente...");
+            }
+        } while (!infoValida);
+
+        infoValida = false;
+        do {
+
+            System.out.println("Digite o seu estado civil entre:s(solteiro) c( casado)  v( viúdo)   d(divorciado)  ");
+            estadoCivil = scan.next();
+
+            switch (estadoCivil) {
+                case "s":
+                case "S":
+                    System.out.println("Estado civil: Solteiro");
+                    break;
+                case "c":
+                case "C":
+                    System.out.println("Estado civil: Casado");
+                    break;
+                case "v":
+                case "V":
+                    System.out.println("Estado civil: Viúvo");
+                    break;
+                case "d":
+                case "D":
+                    System.out.println("Estado civil: Divorviado");
+                    break;
+
+                default:
+                    System.out.println("Estado civil inválido");
+            }
+            infoValida = true;
+        } while (!infoValida);
     }
 
 }
 
 
 //System.out.println("Numero de caracterez: " + nome.length());
+
+// Maneira que Loiane fez:
+// -> Ela colocou o uso das flags, em todas as validações. Sera que essa é a melhor forma?
+
+
+//System.out.println("--------------");
+//System.out.println("Seu nome é:" + nome);
+//System.out.println("Sua idade é:" + idade + " anos");
+//System.out.println("Seu salário é: R$" + salario + " reais");
+//System.out.println("Seu sexo é:" + sexo );
+//System.out.println("Seu estado civil é :" + estadoCivil);
--- exemplosDeCodigoJava/src/aula17/atividades/atividade04.java
+++ exemplosDeCodigoJava/src/aula17/atividades/atividade04.java
@@ -3,6 +3,7 @@ package aula17.atividades;
 public class atividade04 {
     public static void main(String[] args) {
 
+        System.out.println();
 
 
     }
