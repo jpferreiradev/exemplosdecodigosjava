b4870a76449bec268eb9ebe2b769d33315dc35f9
JP Ferreira <jotepauder01@gmail.com>
2023-04-03 15:17:51 -0300

    Update curso Java

--- exemplosDeCodigoJava/src/javaBasico/TesteTres.java
+++ exemplosDeCodigoJava/src/javaBasico/TesteTres.java
@@ -1,9 +1,20 @@
 package javaBasico;
 
+import java.util.Scanner;
+
 public class TesteTres {
     public static void main(String[] args){
-        System.out.println("Você realmente se senti feliz programando em Java :] ");
-        System.out.println("Bora porra!");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scan.nextLine();
+        System.out.println("Digite o seu CPF: ");
+        int cpf = scan.nextInt();
+
+        System.out.println("Seu nome é :" + nome);
+        System.out.println("Seu cpf é:" + cpf);
+
     }
 }
 
