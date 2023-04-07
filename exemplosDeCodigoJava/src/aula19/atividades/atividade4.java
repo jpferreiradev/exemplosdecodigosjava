aa5d66d06ea95771cb5cd563a73e1ee9ba78586e
JP Ferreira <jotepauder01@gmail.com>
2023-04-07 10:04:48 -0300

    Update Java atividades

--- exemplosDeCodigoJava/src/aula19/atividades/atividade4.java
+++ exemplosDeCodigoJava/src/aula19/atividades/atividade4.java
@@ -7,15 +7,27 @@ public class atividade4 {
 
         int a[] = new int[15];
         int b[] = new int[a.length];
+        double vetor[] = new double[a.length];
 
         for(int i = 0;i < a.length;i++){
             System.out.println("Digite um valor na posição: " + i );
             a[i] = scan.nextInt();
-            b[i] = Math.sqrt(a[i]);
-
+            vetor[i] = Math.sqrt(a[i]);
         }
 
-
-
+        for(int i = 0; i < a.length;i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println(" ");
+        for(int i = 0; i < vetor.length; i++){
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
     }
 }
+
+/*
+        int myInt = 9;
+        double myDouble = myInt;
+
+ */
--- exemplosDeCodigoJava/src/aula19/atividades/atividade6.java
+++ exemplosDeCodigoJava/src/aula19/atividades/atividade6.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class atividade6 {
     public static void main(String[] args) {
 
-       // Scanner scan = Scanner(System.in);
+       Scanner scan = new Scanner(System.in);
+
+       int vetorA[] = new int[15];
+       int vetorB[] = new int[vetorA.length];
+
+
+        for(int i = 0;i < vetorA.length;i++){
+            System.out.println("Digite um número inteiro: ");
+
+        }
+
 
     }
 }
