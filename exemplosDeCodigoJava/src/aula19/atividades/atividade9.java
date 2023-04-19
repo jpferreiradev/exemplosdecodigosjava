d5a66a8025f1187d50f304bbca9e958abb73da6a
JP Ferreira <jotepauder01@gmail.com>
2023-04-19 17:35:26 -0300

    Update atividades Java

--- exemplosDeCodigoJava/src/aula19/atividades/atividade9.java
+++ exemplosDeCodigoJava/src/aula19/atividades/atividade9.java
@@ -11,13 +11,14 @@ public class atividade9 {
         int vetorA[] = new int[10];
         int vetorB[] = new int[vetorA.length];
         int vetorC[] = new int[vetorB.length];
+        float vetor[] = new float[vetorC.length];
 
 
         for(int i = 0; i < vetorA.length; i++){
-            System.out.println("Digite uma posição:");
+            System.out.println("Digite uma posição:" + i);
             vetorA[i] = scan.nextInt();
             vetorB[i] = vetorA[i];
-            //vetorC[i] = vetor[i] / float(vetorB[i]);
+            //vetorC[i] = vetorA[i] / vetor[i];
 
         }
     }
