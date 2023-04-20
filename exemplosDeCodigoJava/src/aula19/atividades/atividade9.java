6ea3910f48f5e822cba708300d309640274c5224
JP Ferreira <jotepauder01@gmail.com>
2023-04-20 11:10:59 -0300

    Update atividades Java

--- exemplosDeCodigoJava/src/aula19/atividades/atividade9.java
+++ exemplosDeCodigoJava/src/aula19/atividades/atividade9.java
@@ -8,19 +8,37 @@ public class atividade9 {
 
         Scanner scan = new Scanner(System.in);
 
-        int vetorA[] = new int[10];
+        int vetorA[] = new int[5];
         int vetorB[] = new int[vetorA.length];
-        int vetorC[] = new int[vetorB.length];
-        float vetor[] = new float[vetorC.length];
+        float vetorC[] = new float[vetorB.length];
+        
 
 
         for(int i = 0; i < vetorA.length; i++){
             System.out.println("Digite uma posição:" + i);
             vetorA[i] = scan.nextInt();
             vetorB[i] = vetorA[i];
-            //vetorC[i] = vetorA[i] / vetor[i];
+            vetorC[i] = vetorA[i] / vetorC[i];
 
         }
+
+        System.out.println("Vetor A = ");
+        for(int i = 0; i < vetorA.length;i++){
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Vetor B = ");
+        for(int i = 0; i < vetorB.length;i++){
+            System.out.print(vetorB[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Vetor C = ");
+        for(int i = 0; i < vetorC.length;i++){
+            System.out.print(vetorC[i] + " ");
+        }
+
     }
 
 }
