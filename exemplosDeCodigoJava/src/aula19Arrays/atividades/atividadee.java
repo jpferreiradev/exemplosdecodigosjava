27207a6e5afa4791a5fa4a4532409d4477947890
JP Ferreira <jotepauder01@gmail.com>
2023-05-01 15:29:49 -0300

    Update curso Java

--- exemplosDeCodigoJava/src/aula19Arrays/atividades/atividadee.java
+++ exemplosDeCodigoJava/src/aula19Arrays/atividades/atividadee.java
@@ -3,21 +3,17 @@ package aula19Arrays.atividades;
 import java.util.Scanner;
 
 public class atividadee {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-                                // linha/ coluna
+        // linha/ coluna
         double m[][] = new double[3][3];
 
-        Scanner scan = new Scanner(System.in);
+        //Scanner scan = new Scanner(System.in);
 
-        for(double i = 0; i < 3; i++){
-            for(double j = 0; j < 3; j++){
-                //System.out.println("Digite os valores da matriz" + i);
-                //i = scan.nextDouble();
-                //System.out.println("Digite ");
-                System.out.println("Os números são:" + i  +" , " + j);
-            }
-
+        for (double i = 0; i < 3; i++) {
+            
         }
+
     }
 }
+
