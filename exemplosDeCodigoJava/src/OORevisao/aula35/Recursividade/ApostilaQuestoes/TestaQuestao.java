1aaad5e174af910166fab086864231f9b24f3761
JP Ferreira <jotepauder01@gmail.com>
2023-07-01 12:50:28 -0300

    Add mais questaoes OO revisao

---
+++ exemplosDeCodigoJava/src/OORevisao/aula35/Recursividade/ApostilaQuestoes/TestaQuestao.java
@@ -0,0 +1,12 @@
+package OORevisao.aula35.Recursividade.ApostilaQuestoes;
+
+public class TestaQuestao {
+    public static void main(String [] args ){
+
+
+        for( int numero = 0; numero < 10; numero++){
+            System.out.println(Questao01.primeiraSoma(5));
+        }
+
+    }
+}
