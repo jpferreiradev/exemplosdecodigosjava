a8015d203dee1132e88abee344d555d220fca315
JP Ferreira <jotepauder01@gmail.com>
2023-07-07 12:10:57 -0300

    Update curso revisao Java OO

--- exemplosDeCodigoJava/src/OORevisao/aula36/RelacionamentoEntreClasses/Contato.java
+++ exemplosDeCodigoJava/src/OORevisao/aula36/RelacionamentoEntreClasses/Contato.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package OORevisao.aula36.RelacionamentoEntreClasses;
 
 import com.sun.source.doctree.EndElementTree;
@@ -39,41 +38,4 @@ public class Contato {
 }
 
 
-=======
-package OORevisao.aula36.RelacionamentoEntreClasses;
 
-import com.sun.source.doctree.EndElementTree;
-
-public class Contato {
-
-
-    private String nome;
-    private Endereco endereco;
-    private String telefone;
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Endereco getEndereco(){
-        return this.endereco;
-    }
-
-    public void setEndereco(Endereco endereco){
-        this.endereco = endereco;
-    }
-
-    public String getTelefone() {
-        return this.telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-}
->>>>>>> main
--- exemplosDeCodigoJava/src/OORevisao/aula36/RelacionamentoEntreClasses/Telefone.java
+++ exemplosDeCodigoJava/src/OORevisao/aula36/RelacionamentoEntreClasses/Telefone.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package OORevisao.aula36.RelacionamentoEntreClasses;
 
 public class Telefone {
@@ -28,34 +27,3 @@ public class Telefone {
     }
 
 }
-=======
-package OORevisao.aula36.RelacionamentoEntreClasses;
-
-public class Telefone {
-
-    private String tipo;
-    private String ddd;
-    private String numero;
-
-    public String getTipo(){
-        return this.tipo;
-    }
-    public void setTipo(String tipo){
-        this.tipo = tipo;
-    }
-    public String getDdd(){
-        return this.ddd;
-    }
-    public void setDdd(String ddd){
-        this.ddd = ddd;
-    }
-
-    public String getNumero(){
-        return this.numero;
-    }
-    public void setNumero(String numero){
-        this.numero = numero;
-    }
-
-}
->>>>>>> main
