1f20721d282560b98d3ebc3a8c5e84aabf918a58
JP Ferreira <jotepauder01@gmail.com>
2023-08-11 17:01:18 -0300

    Update revisao OO Java

--- exemplosDeCodigoJava/src/OORevisao/ExceptionsMultiplosCatch/MultiplosCatch.java
+++ exemplosDeCodigoJava/src/OORevisao/ExceptionsMultiplosCatch/MultiplosCatch.java
@@ -7,13 +7,15 @@ public class MultiplosCatch {
 
 
         int[] numeros = {4, 8, 16, 32, 64, 128};
-        int[] denominador = {2, 0, 4, 8, 0};
+        int[] denominadores = {2, 0, 4, 8, 0};
 
         for (int i = 0; i < numeros.length; i++) {
             try {
-                System.out.println(numeros[i] + "/" + denominador[i] + "=" + (numeros[i] / denominador[i]));
-            } catch (ArithmeticException exception) {
-                System.out.println("Pode deixar passar esse erro...");
+                System.out.println(numeros[i] + "/" + denominadores[i] + "=" + (numeros[i] / denominadores[i]));
+            } catch (ArithmeticException e) {
+                System.out.println("Erro ao dividir por zero");
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Posição do array inválida");
             }
 
         }
---
+++ exemplosDeCodigoJava/src/OORevisao/ExceptionsMultiplosCatch/MultiplosCatchJava7.java
@@ -0,0 +1,19 @@
+package OORevisao.ExceptionsMultiplosCatch;
+
+public class MultiplosCatchJava7 {
+    public static void main(String[] args) {
+        int[] numeros = {4, 8, 16, 32, 64, 128};
+        int[] denominadores = {2, 0, 4, 8, 0};
+
+        for (int i = 0; i < numeros.length; i++) {
+            try {
+                System.out.println(numeros[i] + "/" + denominadores[i] + "=" + (numeros[i] / denominadores[i]));
+            } catch (ArithmeticException | ArrayIndexOutOfBoundsException exc) {
+                System.out.println("Aconteceu um erro");
+            }
+
+        }
+
+    }
+}
+
